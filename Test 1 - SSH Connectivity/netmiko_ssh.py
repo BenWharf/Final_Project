7753d1af59e2bc47abc15523f4a662f10229b000
Ben Mills <bengeorgemills@gmail.com>
2020-04-26 19:35:40 +0100

    Added Test 1 Script Comments

--- Test 1 - SSH Connectivity/netmiko_ssh.py
+++ Test 1 - SSH Connectivity/netmiko_ssh.py
@@ -1,8 +1,12 @@
-#1 Router SSH
+#****************
+#* 1 Router SSH *
+#****************
 import netmiko, time
 
+#Start in-code timer
 start = time.time()
 
+#Connect to the router using these details
 router = netmiko.ConnectHandler(
     "192.168.0.1",
     username="admin",
@@ -10,17 +14,25 @@ router = netmiko.ConnectHandler(
     device_type="cisco_ios",
 )
 
+#Send success message to the screen
 print("Router Connected")
 
+#Disconnect from the router
 router.disconnect()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
 
-#2 routers SSH
+#****************
+#* 2 Router SSH *
+#****************
 import netmiko, time
 
+#Start in-code timer
 start = time.time()
 
+#Put the router information in Dictionaries
+#that are placed into a Python list
 two_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -33,21 +45,30 @@ two_routers = [{
     "device_type":"cisco_ios"
     }]
 
+#Run through the python list in a loop
 for router in two_routers:
 
+    #Connect to the router in the current loop
     connected = netmiko.ConnectHandler(**router)
 
+    #Send success message to the screen
     print("Router Connected")
 
+    #Disconnect from router
     connected.disconnect()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
 
-#3 Router SSH
+#****************
+#* 3 Router SSH *
+#****************
 import netmiko, time
 
 start = time.time()
 
+#Put the router information in Dictionaries
+#that are placed into a Python list
 three_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -65,12 +86,17 @@ three_routers = [{
     "device_type":"cisco_ios"
     }]
 
-for router in three_routers:
+#Run through the python list in a loop
+for router in two_routers:
 
+    #Connect to the router in the current loop
     connected = netmiko.ConnectHandler(**router)
 
+    #Send success message to the screen
     print("Router Connected")
-    
+
+    #Disconnect from router
     connected.disconnect()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
--- Test 1 - SSH Connectivity/paramiko_ssh.py
+++ Test 1 - SSH Connectivity/paramiko_ssh.py
@@ -1,31 +1,45 @@
-#1 Router SSH
+#****************
+#* 1 Router SSH *
+#****************
 import paramiko, time
 
+#Start in-code timer
 start = time.time()
 
+#Create the Paramiko SSH Client
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+#Connect to the router using these details
 router = client.connect(
     "192.168.0.1",
     username="admin",
     password="admin"
 )
 
+#Send success message to the screen
 print("Router Connected")
 
+#Disconnect from router
 client.close()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
 
-#2 Router SSH
+#****************
+#* 2 Router SSH *
+#****************
 import paramiko, time
 
+#Start in-code timer
 start = time.time()
 
+#Create the Paramiko SSH Client
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+#Put the router information in Dictionaries
+#that are placed into a Python list
 two_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -38,24 +52,35 @@ two_routers = [{
     "device_type":"cisco_ios"
     }]
 
+#Run through the python list in a loop
 for router in two_routers:
 
+    #Connect to the router in the current loop
     client.connect(router['host'], username=router['username'], password=router['password'])
 
+    #Send success message to the screen
     print("Router Connected")
 
+    #Disconnect from router
     client.close()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
 
-#3 Router SSH
+#****************
+#* 3 Router SSH *
+#****************
 import paramiko, time
 
+#Start in-code timer
 start = time.time()
 
+#Create the Paramiko SSH Client
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+#Put the router information in Dictionaries
+#that are placed into a Python list
 three_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -73,12 +98,17 @@ three_routers = [{
     "device_type":"cisco_ios"
     }]
 
+#Run through the python list in a loop
 for router in three_routers:
 
+    #Connect to the router in the current loop
     client.connect(router['host'], username=router['username'], password=router['password'])
 
+    #Send success message to the screen
     print("Router Connected")
 
+    #Disconnect from router    
     client.close()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
